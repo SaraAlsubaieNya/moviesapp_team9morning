306ec1bb726a26bafa0673486e5cf8296ae09bf1
SaraAlsubaieNya <saralsubaieph@gmail.com>
2025-12-23 10:07:06 +0300

    Initial Commit

---
+++ MoviesAppTests/MoviesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppTests
+//
+//  Created by Sara Alsubaie on 23/12/2025.
+//
+
+import Testing
+@testable import MoviesApp
+
+struct MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
